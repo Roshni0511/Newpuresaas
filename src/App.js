f06a2732caf94a7dc2b20b132c93eac5ea692124
Roshni0511 <roshnichauhan051@gmail.com>
2025-10-09 12:48:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
   <>
   <Routes>
-    <Route />
+    <Route path='/' element={<Home/>}/>
   </Routes>
   </>
   );
