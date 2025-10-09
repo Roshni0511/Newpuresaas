26c12cd6dbbdea1f17a025fd9a5d2a371708daa7
Roshni0511 <roshnichauhan051@gmail.com>
2025-10-09 13:46:44 +0530

    set

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import RealEstateAgentCRM from './Pages/RealEstateAgentCRM';
 
 function App() {
   return (
   <>
   <Routes>
     <Route path='/' element={<Home/>}/>
+    <Route path='/RealEstateAgentCRM' element={<RealEstateAgentCRM/>}/>
   </Routes>
   </>
   );
