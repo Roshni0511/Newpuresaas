25c951b35ba9a51a21bedb57041c6ea8ea516827
Roshni0511 <roshnichauhan051@gmail.com>
2025-10-15 16:38:24 +0530

    set

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Blogdetails from './Pages/Blogdetails';
 import B2BTextileERP from './Pages/B2BTextileERP';
 import Realestatecrm from './Pages/Realestatecrm';
 import OfficeErp from './Pages/OfficeErp';
+import TelecallerCRM from './Pages/Telecallercrm';
 
 const FabButton = ({ id, label, iconClass, link, show, positionClass }) => {
     return (
@@ -170,6 +171,7 @@ function App() {
     <Route path='/B2BTextileERP' element={<B2BTextileERP/>}/>
     <Route path='/Realestatecrm' element={<Realestatecrm/>}/>
     <Route path='/OfficeErp' element={<OfficeErp/>}/>
+    <Route path='/Telecallercrm' element={<TelecallerCRM/>}/>
   </Routes>
   <BackToTopButton />
    <div class="wh-api">
